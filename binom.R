4acf2b71cb75c6b2ae486301f0f5fa4aefb55e99
kgmccann <31707840+kgmccann@users.noreply.github.com>
2020-07-11 16:33:12 -0400

    completed bayesian update for binomial distribution from scratch

--- binom.R
+++ binom.R
@@ -1,30 +1,32 @@
 
 f <- function(x) factorial(x)
 liklihood <- function(w,l,p){
-  f(w+l)/(f(w)*f(l))*p*(1-p)
+  (f(w+l)/(f(w)*f(l)))*p**w*(1-p)**l
 }
 update_prior <- function(new,prior){
-  #'every new observation @param new
+  #'for all new observation @param new
   #'update @param prior
-  #if(!is.null(nrow(new))) new <- colSums(new)
-  liklihood(new[1],new[2],prior[1])*prior[1]/1
+  if(!is.null(nrow(new))) new <- colSums(new)
+  post <- liklihood(new[1],new[2],seq(0,1,length.out=length(prior)))*(prior)
   
+  st_post <- post/sum(post)
+  names(st_post) <- round(seq(0,1,length.out=length(prior)),2)
+  st_post
 }
-
-
-update_prior(c(1,0),c(0.5,0.5))
 
 W <- c(1,0,1,1,1,0,1,0,1)
 L <- (W-1)*-1
 
 N <- cbind(W,L)
-P <- 0.5
+P <- rep(0.5,20)
 
-p_W <- c(0)
-for(i in 1:nrow(N)){
-  P <- update_prior(N[i,],c(P,(1-P)))
-  p_W <- c(p_W,P)
-}
-plot(p_W)
+liklihood(1,0,seq(0,1,length.out=length(P)))==dbinom(1,size=1,prob=seq(0,1,length.out=length(P))) #close enough
 
-dbinom(2,size=2,prob=0.5)
+par(mfrow=c(2,1))
+first_ob <- update_prior(c(1,0),P)
+plot(names(first_ob),first_ob,type='l',col='blue',xlab = '')
+sum(update_prior(c(1,0),P))
+
+plot(names(first_ob),update_prior(N,P),col='red',xlab='Proportion')
+sum(update_prior(N,P))
+
